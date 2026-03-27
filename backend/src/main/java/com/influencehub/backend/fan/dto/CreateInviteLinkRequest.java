03f11ac87b3aaaecea659e0486d2d6bbc4f12848
Joonhwan <joonhwan-choi@users.noreply.github.com>
2026-03-27 13:05:52 +0900

    Accept camelCase invite and fan join payloads

--- backend/src/main/java/com/influencehub/backend/fan/dto/CreateInviteLinkRequest.java
+++ backend/src/main/java/com/influencehub/backend/fan/dto/CreateInviteLinkRequest.java
@@ -1,13 +1,16 @@
 package com.influencehub.backend.fan.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotBlank;
 
 public class CreateInviteLinkRequest {
 
     @NotBlank
+    @JsonProperty("title")
     private String title;
 
     @NotBlank
+    @JsonProperty("sourceLabel")
     private String sourceLabel;
 
     public String getTitle() {
--- backend/src/main/java/com/influencehub/backend/fan/dto/FanJoinInviteRequest.java
+++ backend/src/main/java/com/influencehub/backend/fan/dto/FanJoinInviteRequest.java
@@ -1,18 +1,22 @@
 package com.influencehub.backend.fan.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class FanJoinInviteRequest {
 
     @NotBlank
+    @JsonProperty("inviteCode")
     private String inviteCode;
 
     @Email
     @NotBlank
+    @JsonProperty("email")
     private String email;
 
     @NotBlank
+    @JsonProperty("nickname")
     private String nickname;
 
     public String getInviteCode() {
